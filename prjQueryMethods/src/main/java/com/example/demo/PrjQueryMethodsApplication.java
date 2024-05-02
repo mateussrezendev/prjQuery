c302ba4c222685593f2c2e8e3b19d95e176662c9
Mateus de Rezende Santos <133991473+mateussrezendev@users.noreply.github.com>
2024-05-02 10:29:08 -0300

    Update PrjQueryMethodsApplication.java

--- prjQueryMethods/src/main/java/com/example/demo/PrjQueryMethodsApplication.java
+++ prjQueryMethods/src/main/java/com/example/demo/PrjQueryMethodsApplication.java
@@ -1,13 +1,54 @@
-package com.example.demo;
+package com.example.demo.services;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
-@SpringBootApplication
-public class PrjQueryMethodsApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrjQueryMethodsApplication.class, args);
-	}
+import com.example.demo.entities.Livro;
+import com.example.demo.repositories.LivroRepository;
 
+@Service
+public class LivroService {
+    private final LivroRepository livroRepository;
+
+    @Autowired
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public Livro saveLivro(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public Livro getLivroById(Long id) {
+        return livroRepository.findById(id).orElse(null);
+    }
+
+    public List<Livro> getAllLivros() {
+        return livroRepository.findAll();
+    }
+
+    public void deleteLivro(Long id) {
+        livroRepository.deleteById(id);
+    }
+    
+    // Query buscar por titulo service
+    public List<Livro> buscarPorTitulo (String titulo) {
+    	return livroRepository.buscarPorTitulo(titulo);
+    }
+    
+    public List<Livro> buscaPorAutor(String autor) {
+    	return livroRepository.findByAutor(autor);
+    }
+    public List<Livro> buscarPorNome (String nome) {
+    	return livroRepository.buscarPorNome(nome);
+    }
+    
+    public List<Livro> buscaPorEditora(String editora) {
+    	return livroRepository.buscarPorEditora(editora);
+    }
+    public List<Livro> buscaPorISBN(String isbn) {
+    	return livroRepository.buscarPorISBN(isbn);
+    }
 }
